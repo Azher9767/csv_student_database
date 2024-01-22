8edaa4443e54cac97afe58a172d077a95de55cee
Azher Shaikh <m.azhershaikh9767@gmail.com>
2024-01-22 20:08:42 +0530

    customer feedback exercise complete

--- spec/customer_feedback_spec.rb
+++ spec/customer_feedback_spec.rb
@@ -14,37 +14,56 @@ RSpec.describe "customer feedback data" do
   end
 
   #Calculate the average feedback score.
-  #Find the customer with the shortest response time and their corresponding feedback score.
-  context "average score" do 
+  context "Customer score" do 
     def average_score
-      time = {}
-      time_array = []
-      csv_data.each do |row| 
-        # res_time = row["ResponseTime"].to_i
-        # feedback_score = row["FeedbackScore"]
-        # customer_id = row["CustomerID"]
-          time["feedback_score"] = row["FeedbackScore"].to_i
-           time["response_time"] = row["FeedbackScore"]
-           time["customer_ID"] = row["CustomerID"]
-           time.each do |key, value|
-            time_array << value
-
-        
+      total_feedback_score = 0
+      csv_data.each do |row|
+        feedback_score = row["FeedbackScore"].to_i
+        total_feedback_score += feedback_score
       end
-      time
+      avg_feeback_score = total_feedback_score.to_f / csv_data.length
     end
-    it "" do
-      expect(average_score).to eq(["C005" "FeedbackScore" "ResponseTime"])
+      # time = {}
+      # time_array = []
+      # csv_data.each do |row| 
+      #   # res_time = row["ResponseTime"].to_i
+      #   # feedback_score = row["FeedbackScore"]
+      #   # customer_id = row["CustomerID"]
+      #     time["feedback_score"] = row["FeedbackScore"].to_i
+      #     time["response_time"] = row["FeedbackScore"]
+      #     time["customer_ID"] = row["CustomerID"]
+      #     time.each do |key, value|
+      #     time_array << value
+
+    xit "average score" do
+      expect(average_score).to eq(3.6)
+    end
+  end
+
+  #Find the customer with the shortest response time and their corresponding feedback score.
+  context "response time and feedback score" do
+    def response_time
+    #   csv_data.min_by do |element| 
+    #     element["ResponseTime"].to_i
+    #   end.to_h
+    # end
+      response_time = nil
+      shortest_response_time = nil
+      customer_id = nil
+      feedback_score = nil
+      csv_data.each do |row|
+        response_time = row["ResponseTime"]
+        if shortest_response_time.nil? || response_time < shortest_response_time
+          shortest_response_time = response_time
+          customer_id = row['CustomerID']
+          feedback_score = row['FeedbackScore'].to_i
+        end
+      end
+      [feedback_score, response_time] 
+    end
+
+    it "shortest resposnse time" do
+      expect(response_time).to eq([4, "12"])   #{"CustomerID"=>"C005", "FeedbackScore"=>"4", "ResponseTime"=>"12"}
     end
   end
 end
-
-# CustomerID,  FeedbackScore,  ResponseTime
-#   C001,            4 ,             24
-#   C002,            5,              48
-#   C003,            3               72
-#   C004,            2,              36
-#   C005,            4,              12
-#[#<CSV::Row "CustomerID":"C001" "FeedbackScore":"4" "ResponseTime":"24">, 
-#<CSV::Row "CustomerID":"C0...":"2" "ResponseTime":"36">,
- #<CSV::Row "CustomerID":"C005" "FeedbackScore":"4" "ResponseTime":"12">]
